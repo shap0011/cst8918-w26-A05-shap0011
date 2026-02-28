1ae48eee13171e7039c8d276f666c1f10d8ca1da
shap0011 <shap0011@algonquinlive.com>
2026-02-27 20:42:18 -0500

    add subnet

--- main.tf
+++ main.tf
@@ -76,3 +76,14 @@ resource "azurerm_virtual_network" "vnet" {
 
   address_space = ["10.0.0.0/16"]
 }
+
+# -------------------------
+# Subnet
+# -------------------------
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.labelPrefix}-A05-SUBNET"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = ["10.0.1.0/24"]
+}
