78c111aaf7bfb1cb19f196cbbc5b2b0281e0be0b
shap0011 <shap0011@algonquinlive.com>
2026-02-27 22:34:49 -0500

    add demo screenshot and finalize config

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("${path.module}/id_rsa_clean.pub")
+    public_key = trimspace(file("C:/Users/olga_/.ssh/cst8918_a05_rsa.pub"))
   }
 
   os_disk {
