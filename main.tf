1c7d6ba04916d27dd59eb7a3ad491e4e75412cf4
shap0011 <shap0011@algonquinlive.com>
2026-02-27 20:51:07 -0500

    associate network security group to nic

--- main.tf
+++ main.tf
@@ -146,3 +146,5 @@ resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+
