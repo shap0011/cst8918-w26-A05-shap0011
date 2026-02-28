289409b9c92f8b4e3ef7ba012c844f48bae4dddb
shap0011 <shap0011@algonquinlive.com>
2026-02-27 21:07:41 -0500

    add linux virtual machine

--- main.tf
+++ main.tf
@@ -160,3 +160,38 @@ data "cloudinit_config" "init" {
     content      = file("${path.module}/init.sh")
   }
 }
+
+# -------------------------
+# Virtual Machine
+# -------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-A05-VM"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B1s"
+
+  admin_username = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_ed25519.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(data.cloudinit_config.init.rendered)
+}
