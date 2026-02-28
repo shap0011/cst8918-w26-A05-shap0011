ad999a0c543ad1d0f3bb4f84c10ecd76892eb00e
shap0011 <shap0011@algonquinlive.com>
2026-02-27 20:43:46 -0500

    add network security group with ssh and http rules

--- main.tf
+++ main.tf
@@ -87,3 +87,38 @@ resource "azurerm_subnet" "subnet" {
 
   address_prefixes = ["10.0.1.0/24"]
 }
+
+# -------------------------
+# Network Security Group
+# -------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.labelPrefix}-A05-NSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Allow SSH (Port 22)
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTP (Port 80)
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
