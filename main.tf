d4a24823a5e2c8f10899ba1636e88f93fc9e4956
shap0011 <shap0011@algonquinlive.com>
2026-02-27 20:45:58 -0500

    add network interface

--- main.tf
+++ main.tf
@@ -122,3 +122,19 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
+
+# -------------------------
+# Network Interface (NIC)
+# -------------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.labelPrefix}-A05-NIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
