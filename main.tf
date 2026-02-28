99d1d0b67fa616cfcc12c287a812b4e3bc6dfd71
shap0011 <shap0011@algonquinlive.com>
2026-02-27 21:54:36 -0500

    change vm size to comply with policy

--- main.tf
+++ main.tf
@@ -168,7 +168,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "${var.labelPrefix}-A05-VM"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1s"
+  size                = "Standard_B2s"
 
   admin_username = var.admin_username
 
